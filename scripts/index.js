bebff56e313bff45f98024c1d507c21333189708
jubaer2222 <139365401+jubaer2222@users.noreply.github.com>
2025-08-13 18:39:05 +0600

    added few awesome features

--- scripts/index.js
+++ scripts/index.js
@@ -26,6 +26,18 @@ const loadVocabularyCards = (level) => {
   })
 }
 
+const loadCardDetails = (cardId) => {
+    console.log(cardId);
+    
+    fetch(`https://openapi.programming-hero.com/api/word/${cardId}`)
+        .then(res => res.json())
+        .then(data => {
+          displayCardDetails(data.data);
+
+         })
+
+}
+
 const displayLevelButtons = (lessons) => {
   console.log(lessons);
   const levelBtnContainer = document.getElementById('level-btn-container');
@@ -46,6 +58,20 @@ const displayVocabularyCards = (elements) =>{
    
   const vocabularyCardsContainer = document.getElementById('vocabularyCardsContainer')
     vocabularyCardsContainer.innerHTML = " ";
+
+     if (elements.length == 0) {
+        vocabularyCardsContainer.innerHTML = `
+         <div class="  flex flex-col col-span-3 mx-auto  justify-center items-center bg-[#F8F8F8] rounded-3xl">
+                  <img class="mt-16 mb-4" src="./assets/alert-error.png" alt="">
+                  <p class="text-sm  font-normal mb-4 hind-Siliguri text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                  <p class="mb-16 text-4xl  font-medium hind-Siliguri text-[#292524]">নেক্সট Lesson এ যান</p>
+                </div>
+        `;
+        
+        return;
+     }
+   
+
   for(const element of elements){
     console.log(element);
     const vocabularyCard = document.createElement('div');
@@ -55,6 +81,10 @@ const displayVocabularyCards = (elements) =>{
     <h2 class="card-title">${element.word}</h2>
     <p class="inter-font text-xl font-medium my-4">Meaning /Pronounciation</p>
     <p>${element.meaning}
+    
+    <div>
+    <button onclick="loadCardDetails(${element.id})" class="btn">J</button>
+    </div>
       
     </div>
   </div>
@@ -63,5 +93,28 @@ const displayVocabularyCards = (elements) =>{
   }
 }
 
+const displayCardDetails = (details) =>{
+console.log(details);
+document.getElementById('card_details').showModal()
+ const cardContainer = document.getElementById('card_container')
+    cardContainer.innerHTML = `
+    
+        
+        <h2 class="text-3xl poppins-font flex gap-2 font-semibold mb-7 items-start">${details.word}  <span class="flex gap-2"><p>(<i class="fa-solid fa-microphone font-semibold"></i></p><p class="text-xl hind-Siliguri font-semibold">:</p>${details.pronunciation}</span>)</h2>
+        <h2 class="text-2xl poppins-font font-semibold mb-2">Meaning</h2>
+        <p class="hind-Siliguri text-2xl font-medium">${details.meaning? `${details.meaning}` :' “অর্থ খুজে পাওয়া যাচ্ছে না" '}</p>
+        <h2 class="text-2xl poppins-font font-semibold mb-2 mt-7">Example</h2>
+        <p class="text-lg poppins-font"> ${details.sentence}</p>
+        <h2 class="text-2xl font-medium hind-Siliguri mt-7">সমার্থক শব্দ গুলো</h2>
+        <h2 class="text-2xl font-medium mt-7">${details.synonyms ? `${details.synonyms?.[0] ? `<p class="btn bg-[#EDF7FF] poppins-font px-4 py-5 rounded-lg border">${details.synonyms[0]}</p>` : ''}
+        ${details.synonyms?.[1] ? `<p class=" btn bg-[#EDF7FF] px-4 py-5 poppins-font rounded-lg border">${details.synonyms[1]}</p>` : ''}
+        ${details.synonyms?.[2] ? `<p class=" btn bg-[#EDF7FF] px-4 py-5 rounded-lg poppins-font border">${details.synonyms[2]}</p>` : ''}` : ''} </h2>
+       
+   
+              
+    `
+}
+
 
 loadLevelButtons()
+
